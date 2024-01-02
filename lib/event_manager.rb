129cbe38617c5f6b7aa91bf86435b13e7b279bbe
elayachiamritoff <elayachiamritoff@gmail.com>
2024-01-02 23:09:53 +0100

    Showing all legislators in a zip code Using Google’s Civic info API

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,9 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+
 
 csv_content = CSV.open('../event_attendees.csv', headers: true, header_converters: :symbol)
 
@@ -6,13 +11,38 @@ csv_content = CSV.open('../event_attendees.csv', headers: true, header_converter
        zipcode.to_s.rjust(5, "0")[0..4]
     end
 
+    def legislators_by_zipcode(zip)
 
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+        begin
+            legislators = civic_info.representative_info_by_address(
+            address: zip,
+            levels: 'country',
+            roles:  ['legislatorUpperBody', 'legislatorLowerBody']
+            )
+        legislators = legislators.officials
+
+        legislator_names = legislators.map do |legislator|
+           legislator.name
+        end
+
+        legislator_names.join(",")
+
+        rescue => e
+            "#{e}"
+        end
+    end
 
     csv_content.each do |row|
         name = row[:first_name]
-        zip_code = clean_zipcode(row[:zipcode])
+        zipcode = clean_zipcode(row[:zipcode])
+        legislators_name = legislators_by_zipcode(zipcode)
 
-        puts zip_code
+
+
+        puts "-----------------------------------\nName : #{name}\nZipCode : #{zipcode}\nLegislators : #{legislators_name}\n-----------------------------------\n"
+
+
     end
 
-    csv_content.close
