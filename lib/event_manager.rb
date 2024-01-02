c54f2bfd55dc958375548f5e0e1fa97320bef597
elayachiamritoff <elayachiamritoff@gmail.com>
2024-01-02 15:24:24 +0100

    Add clean_zipcode method to handle zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,10 +2,22 @@ require 'csv'
 
 csv_content = CSV.open('../event_attendees.csv', headers: true, header_converters: :symbol)
 
+    def clean_zipcode(zipcode)
+        if zipcode.nil?
+            "00000"
+        elsif zipcode.length > 5
+           zipcode.slice(0..4)
+        elsif zipcode.length < 5
+            zipcode.rjust(5, "0")
+        else
+            zipcode
+        end
+    end
 
     csv_content.each do |row|
         name = row[:first_name]
-        zip_code = row[:zipcode]
+        zip_code = clean_zipcode(row[:zipcode])
 
-        puts "#{name} #{zip_code}"
     end
+
+    csv_content.close
