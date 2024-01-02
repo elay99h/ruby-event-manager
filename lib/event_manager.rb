184f546bef167bb4702c3615204711bd2419575f
elayachiamritoff <elayachiamritoff@gmail.com>
2024-01-02 23:13:48 +0100

    rubocop -x

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -4,45 +4,37 @@ require 'google/apis/civicinfo_v2'
 civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
 civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
 
-
 csv_content = CSV.open('../event_attendees.csv', headers: true, header_converters: :symbol)
 
-    def clean_zipcode(zipcode)
-       zipcode.to_s.rjust(5, "0")[0..4]
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+
+    legislator_names = legislators.map do |legislator|
+      legislator.name
     end
 
-    def legislators_by_zipcode(zip)
+    legislator_names.join(",")
+  rescue => e
+    "#{e}"
+  end
+end
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
-        begin
-            legislators = civic_info.representative_info_by_address(
-            address: zip,
-            levels: 'country',
-            roles:  ['legislatorUpperBody', 'legislatorLowerBody']
-            )
-        legislators = legislators.officials
+csv_content.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators_name = legislators_by_zipcode(zipcode)
 
-        legislator_names = legislators.map do |legislator|
-           legislator.name
-        end
-
-        legislator_names.join(",")
-
-        rescue => e
-            "#{e}"
-        end
-    end
-
-    csv_content.each do |row|
-        name = row[:first_name]
-        zipcode = clean_zipcode(row[:zipcode])
-        legislators_name = legislators_by_zipcode(zipcode)
-
-
-
-        puts "-----------------------------------\nName : #{name}\nZipCode : #{zipcode}\nLegislators : #{legislators_name}\n-----------------------------------\n"
-
-
-    end
-
+  puts "-----------------------------------\nName : #{name}\nZipCode : #{zipcode}\nLegislators : #{legislators_name}\n-----------------------------------\n"
+end
