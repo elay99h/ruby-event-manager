60d61403f082f5ce36a4b39427da471f3ddfadc4
elayachiamritoff <elayachiamritoff@gmail.com>
2024-01-02 14:57:02 +0100

    output the name of the individual and their zipcode.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,11 @@
-    #test
+require 'csv'
+
+csv_content = CSV.open('../event_attendees.csv', headers: true, header_converters: :symbol)
+
+
+    csv_content.each do |row|
+        name = row[:first_name]
+        zip_code = row[:zipcode]
+
+        puts "#{name} #{zip_code}"
+    end
